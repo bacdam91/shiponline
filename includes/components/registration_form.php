bbf1fd545df2931e861e3682d7019b95315f49fc
bacdam91 <bacdam91@hotmail.com>
2019-11-15 00:43:16 +1100

    Some styling done. Registration handler nearly finish.

--- includes/components/registration_form.php
+++ includes/components/registration_form.php
@@ -1,11 +1,24 @@
-<form action="register.php" method="post">
-    <fieldset>
-        <label for="firstname">Firstname: </label><input type="text" name="firstname" id="firstname" minlength="3" maxlength="50" pattern="<?php echo Pattern::NAME; ?>" required><br />
-        <label for="lastname">Lastname: </label><input type="text" name="lastname" id="lastname" minlength="3" maxlength="50" pattern="<?php echo Pattern::NAME; ?>" required><br />
-        <label for="password_01">Password: </label><input type="password" name="password_01" id="password_01" minlength="10" maxlength="50" pattern="<?php echo Pattern::PASSWORD; ?>" required><br />
-        <label for="password_02">Confirm Password: </label><input type="password" name="password_02" id="password_02" minlength="10" maxlength="50" pattern="<?php echo Pattern::PASSWORD; ?>" required><br />
-        <label for="email">Email Address: </label><input type="email" name="email" id="email" required><br />
-        <label for="phone">Contact Phone: </label><input type="text" name="phone" id="phone" minlength="10" maxlength="10" pattern="<?php echo Pattern::PHONE; ?>" required><br />
-        <input type="submit" value="Register" name="submit">
-    </fieldset>
+<form id="register_form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <label for="firstname">Firstname: </label>
+    <input value="<?php if (isset($firstname)) {
+                        echo $firstname;
+                    } ?>" type="text" name="firstname" id="firstname" minlength="3" maxlength="50" pattern="<?php echo Pattern::NAME; ?>" placeholder="Firstname" required>
+    <label for="lastname">Lastname: </label>
+    <input value="<?php if (isset($lastname)) {
+                        echo $lastname;
+                    } ?>" type="text" name="lastname" id="lastname" minlength="3" maxlength="50" pattern="<?php echo Pattern::NAME; ?>" placeholder="Lastname" required>
+    <label for="password_01">Password: </label>
+    <input type="password" name="password_01" id="password_01" minlength="10" maxlength="50" pattern="<?php echo Pattern::PASSWORD; ?>" placeholder="Password" required>
+    <label for="password_02">Confirm Password: </label>
+    <input type="password" name="password_02" id="password_02" minlength="10" maxlength="50" pattern="<?php echo Pattern::PASSWORD; ?>" placeholder="Re-enter Password" required>
+    <label for="email">Email Address: </label>
+    <input value="<?php if (isset($email)) {
+                        echo $email;
+                    } ?>" type="email" name="email" id="email" placeholder="Email" required>
+    <label for="phone">Contact Phone: </label>
+    <input value="<?php if (isset($phone)) {
+                        echo $phone;
+                    } ?>" type="text" name="phone" id="phone" minlength="10" maxlength="10" pattern="<?php echo Pattern::PHONE; ?>" placeholder="Phone Number" required>
+    <input type="submit" value="Register" name="submit">
+
 </form>
--- includes/handlers/registration_handler.php
+++ includes/handlers/registration_handler.php
@@ -5,28 +5,28 @@ $conn = connectToDatabase($servername, $db, $username, $password);
 $password_criteria = ["1 x uppercase letter", "1 x lowercase letter", "1 x number", "1 of the following special characters (&#33, &#64, &#35, &#36, &#37, &#38)."];
 $lastInsertId = null;
 
-if (isset($_POST["submit"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["firstname"])) {
-        $firstname = filter_var($_POST["firstname"], FILTER_SANITIZE_STRING);
+        $firstname = filter_var(trim($_POST["firstname"]), FILTER_SANITIZE_STRING);
         $firstname = validateInput($firstname, Pattern::NAME);
-        if ($firstname == "") {
+        if (strlen($firstname) == 0) {
             array_push($errors, "<p class=\"error\">Firstname must only contain letters, spaces and apostrophes (&#39) and hyphens (&#45).</p>");
         }
     }
 
     if (isset($_POST["lastname"])) {
-        $lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
+        $lastname = filter_var(trim($_POST["lastname"]), FILTER_SANITIZE_STRING);
         $lastname = validateInput($lastname, Pattern::NAME);
-        if ($lastname == "") {
+        if (strlen($lastname) == 0) {
             array_push($errors, "<p class=\"error\">Lastname must only contain letters, spaces and apostrophes (&#39) and hyphens (&#45).</p>");
         }
     }
 
     if ($_POST["password_01"] == $_POST["password_02"]) {
         if (isset($_POST["password_01"])) {
-            $password_01 = filter_var($_POST["password_01"], FILTER_SANITIZE_STRING);
+            $password_01 = filter_var(trim($_POST["password_01"]), FILTER_SANITIZE_STRING);
             $password_01 = validateInput($password_01, Pattern::PASSWORD);
-            if ($password_01 == "") {
+            if (strlen($password_01) == 0) {
                 $error_message = "<p class=\"error\">Password must contain at least <ul>";
                 foreach ($password_criteria as $criteria) {
                     $error_message .= "<li>$criteria</li>";
@@ -40,24 +40,28 @@ if (isset($_POST["submit"])) {
     }
 
     if (isset($_POST["email"])) {
-        $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        if ($email == "") {
+        if (strlen($email) == 0) {
             array_push($errors, "<p class=\"error\">Invalid email format.</p>");
         }
     }
 
     if (isset($_POST["phone"])) {
-        $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+        $phone = filter_var(trim($_POST["phone"]), FILTER_SANITIZE_STRING);
         $phone = validateInput($phone, Pattern::PHONE);
-        if ($phone == "") {
+        if (strlen($phone) == 0) {
             array_push($errors, "<p class=\"error\">Invalid phone number.</p>");
         }
     }
 
     if (empty($errors)) {
-        $sql = "INSERT INTO Customer (firstname, lastname, password, email, phone) VALUES (?, ?, SHA2(?, 256), ?, ?)";
-        $conn->prepare($sql)->execute([$firstname, $lastname, $password_01, $email, $phone]);
-        $lastInsertId = $conn->lastInsertId();
+        try {
+            $sql = "INSERT INTO Customer (firstname, lastname, password, email, phone) VALUES (?, ?, SHA2(?, 256), ?, ?)";
+            $conn->prepare($sql)->execute([$firstname, $lastname, $password_01, $email, $phone]);
+            $lastInsertId = $conn->lastInsertId();
+        } catch (PDOException $e) {
+            array_push($errors, "<p class=\"error\">" . $e->getMessage() . "</p>");
+        }
     }
 }
